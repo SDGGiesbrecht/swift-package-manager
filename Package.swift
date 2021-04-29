824d9fc67aa04ba358d92397661683425d5b49f8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-04-28 23:40:19 -0600

    Pinned swift‐argument‐parser at 0.4.0.

--- Package.swift
+++ Package.swift
@@ -306,7 +306,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         // The 'swift-argument-parser' version declared here must match that
         // used by 'swift-driver' and 'sourcekit-lsp'. Please coordinate
         // dependency version changes here with those projects.
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/SDGGiesbrecht/swift-driver.git", .exact("0.50400.0")),
     ]
 } else {
